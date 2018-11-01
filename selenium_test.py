06db8ce7075199190132f6f0e4a6071df94ff859
dong-alex <adong@ualberta.ca>
2018-11-01 15:57:39 -0600

    selenium

--- selenium_test.py
+++ selenium_test.py
@@ -2,27 +2,15 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 
+driver = webdriver.Chrome()
+driver.get("http://204.209.76.196:8000")
+
 def test_home():
     # grab the elements for testing
-    driver = webdriver.Chrome()
-    driver.get("http://http://204.209.76.196:8000")
-    driver.implicitly_wait(10)
-    name_element = driver.find_element_by_id("name").text
-    assert name_element != None
-    driver.implicitly_wait(10)
-    about_element = driver.find_element_by_id("about").text
-    assert about_element != None
-    skills_element = driver.find_element_by_id("skills").text
-    education_element = driver.find_element_by_id("education").text
-    work_element = driver.find_element_by_id("work").text
-    contact_element = driver.find_element_by_id("contact").text
-
-    # test the taken given
-    
-    
-    assert skills_element != None
-    assert education_element != None
-    assert work_element != None
-    assert contact_element != None
+    assert driver.find_element_by_id("name") != None
+    assert driver.find_element_by_id("about") != None
+    assert driver.find_element_by_id("skills") != None
+    assert driver.find_element_by_id("education") != None
+    assert driver.find_element_by_id("work") != None
+    assert driver.find_element_by_id("contact") != None
     return
-    
