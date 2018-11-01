207556c34c619ae0cd314b1902b90e71efad841e
dong-alex <adong@ualberta.ca>
2018-11-01 14:20:29 -0600

    selenium

--- selenium_test.py
+++ selenium_test.py
@@ -5,18 +5,20 @@ def test_home():
 
     # grab the elements for testing
     driver = webdriver.Chrome()
-    driver.implicitly_wait(20)
+    driver.implicitly_wait(10)
     driver.get("http://http://204.209.76.196:8000")
     name_element = driver.find_element_by_id("name").text
+    assert name_element != None
     about_element = driver.find_element_by_id("about").text
+    assert about_element != None
     skills_element = driver.find_element_by_id("skills").text
     education_element = driver.find_element_by_id("education").text
     work_element = driver.find_element_by_id("work").text
     contact_element = driver.find_element_by_id("contact").text
 
     # test the taken given
-    assert name_element != None
-    assert about_element != None
+    
+    
     assert skills_element != None
     assert education_element != None
     assert work_element != None
