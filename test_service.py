07c4a3098d623a27d93dfcd38dfcc1a3deb9f6c4
dong-alex <adong@ualberta.ca>
2018-11-01 21:20:04 -0600

    fix tests

--- test_service.py
+++ test_service.py
@@ -4,42 +4,51 @@ from service import Service
 
 class TestOperations(unittest.TestCase):
 
-    @patch("test_service.Service")
+    # path the Service class of the test_service
+    @patch("test_service.Service.bad_random")
     def test_bad_random(self, mock_service):
-        # mock_service now becomes Service
-        # when we .bad_random = 10, we set Service to call bad_random and returns a 10
-
-        mock_service.bad_random = 10
-
-        # assert that Service is called and returns a value
-        value = Service.bad_random
-        assert value == 10
+        mock_service.return_value = 10
+        assert Service.bad_random() == 10
+        mock_service.return_value = 99
+        assert Service.bad_random() == 99
+        mock_service.return_value = 0
+        assert Service.bad_random() == 0
         return
 
-    @patch("test_service.Service")
+    @patch("test_service.Service.bad_random")
     def test_divide(self, mock_service):
-        mock_service.bad_random = 10
-        mock_service.divide = mock_service.bad_random / 2
-        assert Service.divide == 5
+        mock_service.return_value = 12
+        # call the divide fun, iton in service
+        assert Service().divide(2) == 6
+        assert Service().divide(4) == 3
+        assert Service().divide(-2) == -6
+        assert Service().divide(12) == 1
         return
 
     def test_abs_plus(self):
-        negative_value = Service.abs_plus(self, -5)
-        positive_value = Service.abs_plus(self, 1)
-        zero = Service.abs_plus(self, 0)
-        assert negative_value == 6
-        assert positive_value == 2
-        assert zero == 1
+        assert Service.abs_plus(self, -5) == 6
+
+        assert Service.abs_plus(self, 1) == 2
+        assert Service.abs_plus(self, 0) == 1
         return
 
     @patch("test_service.Service")
     def test_complicated_function(self, mock_service):
         # mock the broken random function and previous divide function
-        mock_service.bad_random = 10
-        mock_service.divide = mock_service.bad_random / 2
+        y = 2
+        mock_service.bad_random.return_value = 10
+        mock_service.divide.return_value = mock_service.bad_random() / y
+        mock_service.complicated_function.return_value = Service.divide(y), Service.bad_random() % 2
+        values = Service.complicated_function(2)
+        assert values[0] == 5.0
+        assert values[1] == 0
 
-        mock_service.complicated_function = mock_service.divide, mock_service.bad_random % 2
-        # mock the complicated function
+        y = -2
+        mock_service.divide.return_value = mock_service.bad_random() / y
+        mock_service.complicated_function.return_value = Service.divide(y), Service.bad_random() % 2
+        print(Service.complicated_function(2))
+        assert Service.complicated_function(2)[0] == -5.0
+        assert Service.complicated_function(2)[1] == 0
         return
 
 TestOperations()
